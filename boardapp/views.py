3a6b7bfc4fd54a88c160fe9b9b56655219ac3ee4
Toropon <duck7quick@gmail.com>
2019-12-13 23:22:33 +0900

    change to views.py and signup.html

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 def signupfunc(request):
+  if request.method == "POST":
+    print('this is post method')
+  else:
+    print('this is not post method')
   # コンテキスト{}でモデルを指定して欲しいデータを持ってくる
   return render(request, 'signup.html', {'some':100})
